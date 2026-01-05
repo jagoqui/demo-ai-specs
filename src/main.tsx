21d313cc413e36344a3b1eadec5d98f1f2eee46e
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2026-01-05 18:49:47 -0500

    Adds tailwindcss and TanStack dependencies
    Adds Tailwind CSS for styling and various TanStack libraries (devtools, react-form, react-query, react-router, react-table, react-virtual) to enhance the application's functionality and development experience.
    
    Updates main.tsx to include TanStackDevtools.
    Updates vite config to include tailwindcss plugin.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <TanStackDevtools />
+    {/* <TanStackRouterDevtools router={router} /> */}
+  </StrictMode>
+);
